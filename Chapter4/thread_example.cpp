65c6b6b1895fddeb2b7236d2047fa71c5d1f1945
Idhibhat Pankam <marginpankam@gmail.com>
2026-05-25 18:59:02 +0700

    2

--- Chapter4/thread_example.cpp
+++ Chapter4/thread_example.cpp
@@ -4,7 +4,7 @@ auto dummyFunction(int a, int b, bool sleep) {
   std::cout << "dummyFunction(" << a << "," << b << ")" << std::endl;
   std::cout << "dummyFunction output=" << a + b << std::endl;
 
-  if(sleep) {
+  if (sleep) {
     std::cout << "dummyFunction sleeping..." << std::endl;
 
     using namespace std::literals::chrono_literals;
@@ -17,8 +17,10 @@ auto dummyFunction(int a, int b, bool sleep) {
 int main(int, char **) {
   using namespace Common;
 
-  auto t1 = createAndStartThread(-1, "dummyFunction1", dummyFunction, 12, 21, false);
-  auto t2 = createAndStartThread(1, "dummyFunction2", dummyFunction, 15, 51, true);
+  auto t1 =
+      createAndStartThread(-1, "dummyFunction1", dummyFunction, 12, 21, false);
+  auto t2 =
+      createAndStartThread(1, "dummyFunction2", dummyFunction, 15, 51, true);
 
   std::cout << "main waiting for threads to be done." << std::endl;
   t1->join();
